c940ee2d3eef4eb16ff596b99f812ae223732936
abhishek <purohitabhishek308@gmail.com>
2024-04-30 17:26:45 +0530

    setting up docs and scss

---
+++ src/pages/index.js
@@ -0,0 +1,23 @@
+import Head from "next/head";
+
+export default function Home() {
+  const changeMode = () => {
+    const body = document.querySelector("body");
+    body.classList.toggle("dark");
+    localStorage.getItem("color-theme") === "light"
+      ? localStorage.setItem("color-theme", "light")
+      : localStorage.setItem("color-theme", "dark");
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Abhishek - Frontend Engineer</title>
+
+        <meta name="author" content="Abhishek Purohit" />
+
+        <meta charSet="utf-8" />
+      </Head>
+    </>
+  );
+}
